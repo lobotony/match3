153cab20674c3884c6c61576683ac1dde5738cb3
Tony Kostanjsek <708330+lobotony@users.noreply.github.com>
2019-12-22 21:19:49 +0100

    new gems, drawing a board

--- main.lua
+++ main.lua
@@ -1,32 +1,68 @@
 
 testImage = nil
-panthi = nil
+green = nil
+purple = nil
 
-function love.load() 
-	print("hello once")
-	testImage = love.graphics.newImage("test.png")
-	panthi = love.graphics.newImage("panthi.png")
-	print(testImage)
+gemBitmaps = {}
 
-	love.window.setFullscreen(true)
-	love.mouse.setVisible(false)
+bw = 8
+bh = 8
+bd = {}
+
+sz = 64
+
+bitmapSize = 512
+
+function initBoard() 
+	for i=0,(bw*bh)-1	do
+		bd[i] = i % 3
+	end
 end
 
+function drawGem(gemIndex, x,y)
+	print("gemIndex = "..gemIndex)
+	image = gemBitmaps[gemIndex]
+	scaling = sz / bitmapSize
+	love.graphics.draw(image, 
+		x, 
+		y, 
+		0, 
+		scaling, scaling, 
+		0, 0, 0, 0)
+end
 
-t = 0
+function drawBoard(x, y)
+	for i=0,(bw*bh)-1 do
+		drawGem(bd[i], x+(i%bw)*sz, y+math.floor(i/bw)*sz)
+	end
+end
+
+function love.load() 	
+	print("--- starting")
+
+	initBoard()
+
+	gemBitmaps[0] = love.graphics.newImage("green.png")
+	gemBitmaps[1] = love.graphics.newImage("purple.png")
+	gemBitmaps[2] = love.graphics.newImage("blue.png")
+--	love.window.setFullscreen(true)
+--	love.mouse.setVisible(false)
+end
+
+function love.update(dt)
+	if(love.mouse.isDown(1)) then
+		print("down")
+	end
+end
+
 function love.draw()
 
-	love.graphics.draw(panthi, 0, 0, 0, .2, .2, 0, 0, 0, 0)
+	-- center in window 
+	w,h = love.graphics.getDimensions( )
+	bpw = sz*bw
+	bph = sz*bh
+	ox = (w - bpw)/2
+	oy = (h - bph)/2
 
-	mx, my = love.mouse.getPosition()
-
-	st = math.sin(t)
-
-	love.graphics.draw(testImage, 
-		mx, 
-		my+st*10, 
-		0, 
-		0.1, 0.1, 
-		0, 0, 0, 0)
-	t = t + .2
+	drawBoard(ox,oy)
 end
