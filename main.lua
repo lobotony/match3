9ad7325351cfddb3e68f2cba0f8f8e83b7b9596b
Tony Kostanjsek <708330+lobotony@users.noreply.github.com>
2020-02-01 16:13:28 +0100

    added inspect lib and test output

--- main.lua
+++ main.lua
@@ -1,11 +1,13 @@
 local Game = require("Game")
 local game = Game:create()
 
+local inspect = require("inspect/inspect")
 
 function love.load() 	
 	love.window.setMode(800,600,{highdpi=true})
 	print("--- starting")
 	game:init()
+	print(inspect(game, {depth=3}))
 end
 
 --[[
